f18dabd5dcfa2ea60dc08890f04db7f8d904bab5
akzgit <akashtyo@gmail.com>
2023-08-09 16:45:30 +0530

    09-08-2023

--- Django_ex2/ProTwo/AppTwo/form.py
+++ Django_ex2/ProTwo/AppTwo/form.py
@@ -1,10 +1,22 @@
 from django import forms
 from AppTwo.models import Student # or-->from first_app.models import Student
+from django.core import validators
 
 
-class  StudentForm(forms.ModelForm):
-    class Meta:
-        model=Student
-        fields='__all__'  #fields ='var1,var2,var3' here it is all
+# class  StudentForm(forms.ModelForm):
+#     class Meta:
+#         model=Student
+#         fields='__all__'  #fields ='var1,var2,var3' here it is all
+
+
+#user-defined validation
+def check(value):
+    if value[0].lower() != 'a':
+        raise forms.ValidationError("Name need to be started with A")
+    
+
+class StudentForm(forms.Form) :
+      student_name =forms.CharField(validators =[check],max_length=50)
+      student_course = forms.CharField(max_length=50)    
         
         
